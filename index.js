f127ea620692f41d23a944b49e376568a0bbbfe7
mckaymatt3 <mckaymatt@outlook.com>
2022-01-20 16:50:25 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#ff69b4";
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 5}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+   if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+   }
+   if (e.key === "ArrowRight") {
+        moveDodgerRight();
+   }
+});
+
+
+
