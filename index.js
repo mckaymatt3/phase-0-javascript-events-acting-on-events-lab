8cebd9055d9f53f074d1f1b007b8654bb1b3d29f
mckaymatt3 <mckaymatt@outlook.com>
2022-02-06 13:22:25 -0500

    commit tree 6663ec0c86574d0382d1fade695317483fa34f1c

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+const dodger = document.querySelector('#dodger');
 
-dodger.style.backgroundColor = "#ff69b4";
+dodger.style.backgroundColor = "blue";
 
-function moveDodgerLeft() {
-    var leftNumbers = dodger.style.left.replace("px", "");
-    var left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 5}px`;
-    }
+document.addEventListener("keydown", function (event){
+  if (event.key === "Arrow Left") {
+  const leftNumbers = dodger.style.left.replace("px", " ");
+  const left = parsetInt(leftNumbers,10);
+
+  dodger.style.left = `${left - 1}px`
   }
+})
 
-function moveDodgerRight() {
-    var leftNumbers = dodger.style.left.replace("px", "");
-    var left = parseInt(leftNumbers, 10);
+
+// const dodger = document.getElementById("dodger");
+
+// dodger.style.backgroundColor = "#ff69b4";
+
+// // function moveDodgerLeft() {
+// //     var leftNumbers = dodger.style.left.replace("px", "");
+//     var left = parseInt(leftNumbers, 10);
   
-    if (left > 0) {
-      dodger.style.left = `${left + 5}px`;
-    }
-  }
+//     if (left > 0) {
+//       dodger.style.left = `${left - 5}px`;
+//     }
+//   }
 
-document.addEventListener("keydown", function (e) {
-   if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
-   }
-   if (e.key === "ArrowRight") {
-        moveDodgerRight();
-   }
-});
+// function moveDodgerRight() {
+//     var leftNumbers = dodger.style.left.replace("px", "");
+//     var left = parseInt(leftNumbers, 10);
+  
+//     if (left > 0) {
+//       dodger.style.left = `${left + 5}px`;
+//     }
+//   }
+
+// document.addEventListener("keydown", function (e) {
+//    if (e.key === "ArrowLeft") {
+//         moveDodgerLeft();
+//    }
+//    if (e.key === "ArrowRight") {
+//         moveDodgerRight();
+//    }
+// });
 
 
 
